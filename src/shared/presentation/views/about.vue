e05281383af0e4f81b2d19df515ab1c636ebb9a1
Alice-keys <u20231d637@upc.edu.pe>
2025-09-26 16:34:25 -0500

    feat(i18n): integrate i18n translations into about page layout

--- src/shared/presentation/views/about.vue
+++ src/shared/presentation/views/about.vue
@@ -1,9 +1,15 @@
 <script setup>
+import {useI18n} from "vue-i18n";
 
+const { t } = useI18n()
 </script>
 
 <template>
-
+ <div class="align-content-start justify-content-start m-4">
+   <h1>{{ t('about.tittle')}}</h1>
+   <img src="/acme-logo.svg" alt="Acme Logo" width="200" height="200" />
+   <p>{{ t('about.content')}}</p>
+ </div>
 </template>
 
 <style scoped>
