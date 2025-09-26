c344bd24a10676438e07e9a8a072d29a17375605
Alice-keys <u20231d637@upc.edu.pe>
2025-09-26 16:29:45 -0500

    feat(i18n): integrate PrimeVue SelectButton into language switcher component

--- src/shared/presentation/components/language-switcher.vue
+++ src/shared/presentation/components/language-switcher.vue
@@ -1,8 +1,12 @@
 <script setup>
+import {useI18n} from "vue-i18n";
+import {SelectButton as PvSelectButton} from "primevue";
 
+const { t, locale, availableLocales } = useI18n()
 </script>
 
 <template>
+ <pv-select-button v-model="locale" :options="availableLocales"/>
 
 </template>
 
